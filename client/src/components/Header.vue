0b2bef3d29ebe5949fb5383e3ebf2c5c6abddec1
Arvin Rahimi <arvinrahimi78@gmail.com>
2023-10-17 13:37:10 +0200

    #12 Make header scalable
    this commit makes the header scalable with a new implementation

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,87 +1,92 @@
- <template>
-    <header class="fightapp_header">
-      <nav class="header_nav_bar" v-if="isLoggedIn">
-        <a href="#/" class="header_link">Logout</a>
-      </nav>
-      <div class="logo_container">
-        <a href="#/" class = "header_logo">FightApp</a>
+<template>
+  <header>
+    <nav class="navbar navbar-expand-sm background-color header-be-front text-font">
+      <div class="container-fluid">
+        <a href="#" class="navbar-logo text-color">FightApp</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav justify-content-between w-100 me-auto me-lg-0 mb-2 mb-lg-0">
+            <div class="col d-flex flex-wrap justify-content-sm-start justify-content-center">
+              <li class="nav-item">
+              <div v-if="isLoggedIn">
+                <a href="#" class="nav-link navbar-items text-color">Browse Fighters</a>
+              </div>
+            </li>
+            </div>
+            <div class="col">
+              <a href="#" class="fake-navbar-logo text-color">FightApp</a>
+            </div>
+            <div class="col d-flex flex-wrap justify-content-sm-end justify-content-center">
+              <li class="nav-item">
+              <div v-if="isLoggedIn">
+                <a href="#" class="nav-link navbar-items text-color">Logout</a>
+              </div>
+            </li>
+            </div> 
+          </ul>
+        </div>
       </div>
-      <nav class="header_nav_bar" v-if="isLoggedIn">                   
-        <a href="#/" class="header_link">Browse Fighters</a>
-      </nav>
-    </header>
-  </template>
-  
-  <script>
-    export default {
-    name: 'Header',
-    inject: ['isLoggedIn'] // injected from "App.vue"
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+  name: 'Header',
+  inject: ['isLoggedIn'] // injected from "App.vue"
+}
+</script>
+
+<style scoped>                  /* Style scoped applies css properties to HTML elements but only targets the elements of the component it is in. */
+
+@import '../css/fonts.css';
+
+a {
+  color: #ffffff;                      /* Link color */
+  font-size: 4vh;
   }
-  </script>
-  
-  <style scoped>                  /* Style scoped applies css properties to HTML elements but only targets the elements of the component it is in. */
-  
-  @import '../css/fonts.css';
-
-  a {
-    color: #ffffff;                      /* Link color */
-    font-size: 4vh;
-    }
-
-  .fightapp_header {
-    background-color: rgba(255, 0, 0, 0.301);
-    display: flex;               /* Adapting to screen size*/
-    align-items: center;
-    height: 100px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    padding: 20px;
-    z-index: 9999;              /* The high z-index value ensures consistency in that the page header is always displayed above all other page content. */
-    font-family: 'MetalMania';
+.background-color {
+  background-color: rgba(255, 0, 0, 0.301);
+}
+.header-be-front {
+  z-index: 9999;
+}
+.navbar-logo {
+  padding-top: 0.3125rem;
+  padding-bottom: 0.3125rem;
+  padding-left: 0.5rem;
+  margin-right: 2rem;
+  text-decoration: none;
+  font-size: 55px;
+}
+.fake-navbar-logo {
+  text-decoration: none;
+  font-size: 55px;
+}
+.navbar-items {
+  font-size: 30px;
+  transition: transform 0.3s ease-in-out;           /* Defines how the transition between two states will look.  */
+}
+.navbar-items:hover {
+  transform: scale(1.1);
+}
+.text-font {
+  font-family: 'MetalMania';
+}
+.text-color {
+  color: #FFFF00;
+}
+@media (min-width: 576px){
+  .navbar-logo {
+    display: none;
+  }
+}
+@media (max-width: 576px) {
+  .fake-navbar-logo{
+    display: none;
+  }
 }
 
-  .logo_container {
-    display: flex;              /* Adapting to screen size */
-    flex: 1;                    /* Resizes when growing and shrinking */
-    justify-content: center;    /* Centers the flex items horizontally within the container. (The links are always centered )  */
-  }
-  
-  .header_nav_bar {
-    display: flex;              /* Adapting to screen size */
-    flex: 1;                    /* Resizes when growing and shrinking */
-    justify-content: center;    /* Centers the flex items horizontally within the container. (The links are always centered )  */
-  }
-  
-  .header_link {
-    margin: 10px;
-    text-decoration: none;
-    font-size: 40px;
-    color: #FFFF00;
-    -webkit-text-stroke-width: 1px;
-    -webkit-text-stroke-color: black;
-    transition: transform 0.3s ease-in-out;  /* Defines how the transition between two states will look.  */
-  }                                          /* In this case the links will move smoothly in and out between scale 1 and 1.1 when hovered  */
-  
-  .header_logo {
-    margin: 10px;
-    text-decoration: none;
-    font-size: 75px;
-    color: #FFFF00;
-    -webkit-text-stroke-width: 2px;
-    -webkit-text-stroke-color: black;
-    transition: transform 0.3s ease-in-out;  /* Defines how the transition between two states will look.  */
-  }                                          /* In this case the links will move smoothly in and out between scale 1 and 1.1 when hovered  */
-  
-  .header_link:hover {
-    transform: scale(1.1);                   /* Makes element  10 % larger  */
-  }
-  
-  .header_button {
-    margin: 20px;
-    width: 250px;
-  }
-  
-  </style>
-  
+</style>
